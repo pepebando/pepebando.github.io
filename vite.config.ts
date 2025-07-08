475e4b8753a950388059c2bc75b373cacc721ad6
Pepe Obando <jsobando96@hotmail.com>
2025-07-08 10:39:37 -0500

    te

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // necesario para GitHub Pages en el root de usuario
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-    
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },  base:'https://pepebando.github.io/',
-  publicPath: '/'
+  },
 }));
