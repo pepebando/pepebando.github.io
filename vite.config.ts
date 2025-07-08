cf53775cc53df3ee91e6dc552d4a2b45e148d300
Pepe Obando <jsobando96@hotmail.com>
2025-07-08 11:06:14 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,23 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // necesario para GitHub Pages en el root de usuario
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 }));
